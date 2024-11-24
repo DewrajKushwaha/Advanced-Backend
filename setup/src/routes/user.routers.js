06af2f26274306507d5a141f4abdfd9750be3067
Dewraj Kumar Kushwaha <dewrajkumarlochar@gmail.com>
2024-11-24 16:15:14 +0530

    delete some directory

--- setup/src/routes/user.routers.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {registerUserd} from "../controllers/user.controllers"
-const router =Router()
-
-router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
-
-export default router
